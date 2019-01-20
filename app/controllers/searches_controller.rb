18717ca5c15e78b0cd6b43a02edc6714596ec968
Hillevi Eklöw <hillevieklow@gmail.com>
2019-01-20 20:01:17 +0000

    Automatically backed up by Learn

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -30,11 +30,11 @@ class SearchesController < ApplicationController
   end
 
   def friends
-  resp = Faraday.get("https://api.foursquare.com/v2/users/self/friends") do |req|
-    req.params['oauth_token'] = session[:token]
-    # don't forget that pesky v param for versioning
-    req.params['v'] = '20160201'
+    resp = Faraday.get("https://api.foursquare.com/v2/users/self/friends") do |req|
+      req.params['oauth_token'] = session[:token]
+      # don't forget that pesky v param for versioning
+      req.params['v'] = '20160201'
+    end
+    @friends = JSON.parse(resp.body)["response"]["friends"]["items"]
   end
-  @friends = JSON.parse(resp.body)["response"]["friends"]["items"]
-end
 end
